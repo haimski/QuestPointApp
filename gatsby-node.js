c8c7ccf48551b96c241e3aa48b455cefac7a95e0
haimski <benzak@gmail.com>
2026-02-05 12:23:00 +0200

    remove starter ssr/dsg pages to fix vercel output symlink

--- gatsby-node.js
+++ gatsby-node.js
@@ -10,13 +10,6 @@
 exports.createPages = async ({ actions, graphql, reporter }) => {
   const { createPage } = actions
 
-  createPage({
-    path: "/using-dsg",
-    component: require.resolve("./src/templates/using-dsg.js"),
-    context: {},
-    defer: true,
-  })
-
   const result = await graphql(`
     {
       allMarkdownRemark(
